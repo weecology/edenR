e2d4cfe942d9db2510d8d3f22f7fe31af3c0111c
Ethan White <ethan@weecology.org>
2026-03-10 10:47:16 -0400

    Cleanup style throughout code base
    Ran styler across the code base. This makes the code more readable and
    will avoid issues with editors auto-running styler resulting in extra
    style changes showing up in commits/PR's

--- R/edenR-package.R
+++ R/edenR-package.R
@@ -1,12 +1,12 @@
 #' @keywords internal
-#' 
+#'
 #' @title Retrieve and use EDEN data
-#' 
-#' @description The **edenR** package provides functions to retrieve, process and 
+#'
+#' @description The **edenR** package provides functions to retrieve, process and
 #' summarize the EDEN water depth data. The data begin in 1991 and are continuously updated today.
-#' 
+#'
 #' @name edenR
-#' 
+#'
 "_PACKAGE"
 
 ## usethis namespace: start
